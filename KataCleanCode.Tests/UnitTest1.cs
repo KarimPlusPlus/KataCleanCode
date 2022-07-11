3e1a8cab4064fac4edeb5878fc9a579908703702
Karim BOUTOUIL <Karim.boutouil@arolla.fr>
2022-07-11 16:01:25 +0200

    Add multiple command

--- KataCleanCode.Tests/UnitTest1.cs
+++ KataCleanCode.Tests/UnitTest1.cs
@@ -54,6 +54,36 @@ namespace KataCleanCode.Tests
             // Assert
             Check.That(taskManager.Tasks).ContainsExactly("1 [ ] Learn Python", "2 [ ] Learn TDD");
         }
+
+        [Fact]
+        public void StatusOfFirstTask()
+        {
+            // Arrange
+            var taskManager = new TaskManager();
+            taskManager.AddCommand("+ Learn Python");
+            taskManager.AddCommand("+ Learn TDD");
+
+            // Act
+            var result = taskManager.AddCommand("x 1");
+
+            // Assert
+            Check.That(result).IsEqualTo("1 [ ] Learn Python");
+        }
+
+        [Fact]
+        public void StatusOfSecondTask()
+        {
+            // Arrange
+            var taskManager = new TaskManager();
+            taskManager.AddCommand("+ Learn Python");
+            taskManager.AddCommand("+ Learn TDD");
+
+            // Act
+            var result = taskManager.AddCommand("x 2");
+
+            // Assert
+            Check.That(result).IsEqualTo("2 [ ] Learn TDD");
+        }
     }
 
     internal class TaskManager
@@ -64,17 +94,31 @@ namespace KataCleanCode.Tests
 
         public IEnumerable<string> Tasks { get; set; } = Enumerable.Empty<string>();
 
-        internal void AddCommand(string command)
+        public string AddCommand(string command)
         {
-            if (command == "q")
+            var commandLetter = command.First();
+
+            switch (commandLetter)
             {
-                Tasks = Enumerable.Empty<string>();
+                case 'q':
+                    Tasks = Enumerable.Empty<string>();
+                    break;
+                case '+':
+                    {
+                        var taskName = command.Remove(0, 2);
+                        Tasks = Tasks.Append($"{Tasks.Count() + 1} [ ] {taskName}");
+                        break;
+                    }
+
+                case 'x':
+                    {
+                        var positionString = command.Remove(0, 2);
+                        var position = int.Parse(positionString);
+                        return $"{Tasks.ElementAt(position - 1)}";
+                    }
             }
-            else
-            {
-                var taskName = command.Remove(0, 2);
-                Tasks = Tasks.Append($"{Tasks.Count() + 1} [ ] {taskName}");
-            }
+
+            return String.Empty;
         }
     }
 }
